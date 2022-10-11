ff10a4c25e4c66b46384dc7900fc4d1c0fb2a619
Jonathan Ngbonga <jonathan@ntls.io>
2022-10-12 00:25:32 +0200

    feat(auth): add guard to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import {
+  redirectUnauthorizedTo,
+  redirectLoggedInTo,
+  AngularFireAuthGuard,
+  canActivate,
+} from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+
 const routes: Routes = [
   {
     path: '',
@@ -11,11 +21,13 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () =>
       import('./pages/login/login.module').then((m) => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToHome),
   },
   {
     path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomePageModule),
+    ...canActivate(redirectUnauthorizedToLogin),
   },
 ];
 
